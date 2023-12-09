89013e66ebc2ffc0161b5c60dadbb8c92df9f25b
Anastasya <aamokeeva@yandex.ru>
2023-12-09 16:57:41 +0300

    Создает простое приложение-блог

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,117 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <form @submit.prevent>
+      <h1>Создание поста</h1>
+      <input
+        class="input"
+        type="text"
+        placeholder="Название"
+        :value="title"
+        @input="title = $event.target.value"
+      />
+      <input
+        class="input"
+        type="text"
+        placeholder="Описание"
+        :value="description"
+        @input="description = $event.target.value"
+      />
+      <button
+        class="btn"
+        @click="createPost">
+          Создать
+      </button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><b>Название:</b> {{ post.title }}</div>
+      <div><b>Описание:</b> {{ post.description }}</div>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      description: '',
+      posts: [
+        {
+          id: 1,
+          title: "Коронованный герцог графства не позволил союзу развалиться",
+          description:
+            "Банальные, но неопровержимые выводы, а также сделанные на базе интернет-аналитики выводы и по сей день остаются уделом либералов, которые жаждут быть функционально разнесены на независимые элементы.",
+        },
+        {
+          id: 2,
+          title:
+            "Логотип крупнейшей компании по производству мыльных пузырей оправдал надежды граждан",
+          description:
+            "Противоположная точка зрения подразумевает, что интерактивные прототипы представляют собой не что иное, как квинтэссенцию победы маркетинга над разумом и должны быть ограничены исключительно образом мышления.",
+        },
+        {
+          id: 3,
+          title: "Коронованный герцог графства продолжает удивлять",
+          description:
+            "Каждый из нас понимает очевидную вещь: курс на социально-ориентированный национальный проект однозначно определяет каждого участника как способного принимать собственные решения касаемо экспериментов, поражающих по своей масштабности и грандиозности.",
+        },
+      ],
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        description: this.description
+      }
+
+      this.posts = [newPost, ...this.posts];
+      this.title = '';
+      this.description = '';
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.post {
+  margin-top: 15px;
+  padding: 15px;
+  border: 2px solid teal;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  margin-top: 15px;
+  padding: 10px 15px;
+  border: 2px solid teal;
+  border-radius: 5px;
+}
+
+.btn {
+  margin-top: 15px;
+  padding: 10px 15px;
+  align-self: flex-end;
+  color: teal;
+  border: 1px solid teal;
+  border-radius: 5px;
+  background: none;
+  cursor: pointer;
 }
 </style>
